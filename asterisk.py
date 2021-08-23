fc9e816d1e7ad36a4a68536452bbc61609088aa5
CatalystM47 <catalystm47@gmail.com>
2021-08-23 17:17:53 +0900

    my first time

---
+++ asterisk.py
@@ -0,0 +1,10 @@
+def print_more(req1, req2, *args):
+	print('First need is: ', req1)
+	print('Second need is: ', req2)
+	print('Other needs are: ', args)
+
+print_more('cap', 'gloves', 'scarf', 'monocle', 'mustach wax')
+
+
+
+
